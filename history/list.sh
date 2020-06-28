e33c62d5fd9a1c9b9286bb2cc26383f8e9edb88d
LiHua-Official <65519031+LiHua-Official@users.noreply.github.com>
2020-06-28 04:16:41 -0500

    Update list.sh

--- history/list.sh
+++ history/list.sh
@@ -3,5 +3,5 @@ echo '# 在git add 前运行 bash list.sh 以更新README.md' > README.md
 echo '# 下载地址:' >> README.md
 for filename in `ls *.apk`
 	do
-		echo "[${filename%.*}](https://coolapk-fan.github.io/wiki/history/$filename)" >>README.md
+		echo "[${filename%.*}](https://coolapk-fan.github.io/HistoryVersions/history/$filename)" >>README.md
 	done
